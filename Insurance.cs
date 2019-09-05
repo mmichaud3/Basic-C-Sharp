2791836e665a4ceff525dd5dcb361913dc01c041
Michael <mmichaud3@yahoo.com>
2019-09-05 16:06:00 -0700

    step 75

---
+++ Insurance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int newAge = Convert.ToInt16(age);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI? Answer 'true' or 'false'");
+            string dui = Console.ReadLine();
+            bool duiAnswer = Convert.ToBoolean(dui);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            int totalTickets = Convert.ToInt16(tickets);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualafied?");
+            bool qualified = (newAge > 15) && (duiAnswer == false) && (totalTickets <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
