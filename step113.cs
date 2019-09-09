c419431ec5ab5f0da8fc212279c3bd99beb73ed7
Michael <mmichaud3@yahoo.com>
2019-09-09 16:27:26 -0700

    step 113

---
+++ step113.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Class1();
+            int x = 10;
+            int y = 5;
+            Console.WriteLine("The product is : " + p.meth1(x, y));
+            Console.ReadLine();
+        }
+    }
+}
