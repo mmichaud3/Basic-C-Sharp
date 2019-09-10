59bd80e8b80eddb59b16d941291b15f24c75605d
Michael <mmichaud3@yahoo.com>
2019-09-10 15:11:43 -0700

    step 115

---
+++ drill115/drill115/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill115
+{
+    public class Class1
+    {
+        public int Add(int a, int b = 10)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ drill115/drill115/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Class1();
+            Console.WriteLine("Select two numbers one at a time or leave second blank");
+            int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of your numbers is: " + x.Add(a ));
+            Console.ReadLine();
+
+
+        }
+    }
+}
