8f9ac1942ef1cd16203455dac8dd1dd07e8a5a99
Michael <mmichaud3@yahoo.com>
2019-09-10 12:48:56 -0700

    update project 113

---
+++ step113/step113/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step113
+{
+    public class Class1
+    {
+        public int meth1(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+    }
+}
