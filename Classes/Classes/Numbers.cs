160c07c5bd71f44377fb313fc61aab3e02a099fc
Michael <mmichaud3@yahoo.com>
2019-09-10 12:54:50 -0700

    update classes

---
+++ Classes/Classes/Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Numbers
+    {
+        public int Factorial(int n)
+        {
+            int f = 1;
+
+            for (int i = 1; i<= n; i++)
+            {
+                f = f * i;
+            }
+            return f;
+        }
+        public int Sum(int x, int y)
+        {
+            int a = x;
+            int b = y;
+
+            int result = a + b;
+            return result;
+        }
+        public int Product(int x, int y)
+        {
+            int d = x;
+            int e = y;
+
+            int result = d * e;
+            return result;
+        }
+
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Select a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var f = new Numbers();
+            Console.WriteLine("The Factorial of your number is: " + f.Factorial(num));
+            Console.ReadLine();
+
+            var s = new Numbers();
+            int a = num;
+            int b = 5;
+            Console.WriteLine("The sum of your number and 5 is: " + s.Sum(num, b));
+            Console.ReadLine();
+
+            var p = new Numbers();
+            int d = num;
+            int e = 5;
+            Console.WriteLine("The product of your number and 5 is: " + p.Product(num, e));
+            Console.ReadLine();
+        }
+    }
+}
