bfb036f5194c1cc0e0cee6b37a1b52ab66b5f10e
Michael <mmichaud3@yahoo.com>
2019-09-10 16:18:35 -0700

    drill 113

---
+++ drill113/drill113/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill113
+{
+    public class Class1
+    {
+        public void Meth3(int x, int y)
+        {
+            int z = x + 10;
+            Console.WriteLine(y);
+
+        }
+    }
+}
---
+++ drill113/drill113/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Class1();
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(j.Meth3(x,y));
+        }
+    }
+}
