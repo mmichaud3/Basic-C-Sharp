26ab3ee9ffa7ee9f96ccd468b78c10c39dc6df3a
Michael <mmichaud3@yahoo.com>
2019-09-10 08:24:53 -0700

    step 34

--- AcmeAccounting.cs
+++ AcmeAccounting.cs
@@ -8,7 +8,7 @@
         static void Main()
         {
         Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remeber, we're accounting on you!");
+        Console.WriteLine("Remeber, we're \"accounting\" on you!");
         Console.ReadLine();
         }
     }
