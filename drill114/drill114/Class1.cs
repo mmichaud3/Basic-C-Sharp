5bb84569a606df4f7d14750a58df33b3446a8332
Michael <mmichaud3@yahoo.com>
2019-09-10 14:00:26 -0700

    drill 114

---
+++ drill114/drill114/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill114
+{
+   public class Class1
+    {
+        public int Sum(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
---
+++ drill114/drill114/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Class1();
+            int x = 100;
+            int y = 50;
+            Console.WriteLine("The sum of 100 and 150 is: " + c.Meth1(x, y));
+            Console.ReadLine();
+
+            var e = new Class1();
+            decimal a = 2.2m;
+            decimal b = 5.5m;
+            Console.WriteLine(" The product rounded down is : " + e.Meth1(a, b));
+            Console.ReadLine();
+
+            var s = new Class1();
+            string g = "two";
+            int h = 2;
+            int d = 20;
+            Console.WriteLine("The Quotient is : " + s.Meth1(d, h));
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
