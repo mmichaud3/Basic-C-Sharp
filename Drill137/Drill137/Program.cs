7e8bb69f1e35f4a746e20bf6f848a52a13f4b7fc
Michael <mmichaud3@yahoo.com>
2019-09-18 16:55:57 -0700

    step 137

---
+++ Drill137/Drill137/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();//instatiating new number
+            number.Amount = 3.5m; // assiging value
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
