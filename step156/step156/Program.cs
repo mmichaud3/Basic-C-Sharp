06a49911b09d660980fdfd1ffafdb2b0b1a4e31e
Michael <mmichaud3@yahoo.com>
2019-09-23 13:10:00 -0700

    step 156

---
+++ step156/step156/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("enter a number.");
+            int X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(DateTime.Now.AddHours(X));
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
