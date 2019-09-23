f0ea49556605e3332d079d65fa16e1e400625f84
Michael <mmichaud3@yahoo.com>
2019-09-23 12:22:26 -0700

    step 154

---
+++ step154/step154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string num1 = Convert.ToString(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\thetechacademy\step154text.txt", num1);
+            File.ReadAllText(@"C:\Users\thetechacademy\step154text.txt");
+            Console.WriteLine(num1);
+            Console.ReadLine();
+        }
+    }
+}
