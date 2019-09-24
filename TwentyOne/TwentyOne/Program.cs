720d39251f71710eba692587976215290530a0d8
Michael <mmichaud3@yahoo.com>
2019-09-23 18:33:56 -0700

    step 165

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -13,8 +13,15 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}.  Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
@@ -31,7 +38,29 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person put.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (ArgumentException)
+                    {
+                        Console.WriteLine("Something you entered was incorrect.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occurred.  Please contact your System Administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    game -= player;
                     Console.WriteLine("Thank you for playing!");
                 }
                 Console.WriteLine("Feel free to look around the casino.  Bye for now.");
---
+++ step165/step165/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace step165
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            var dobYear = DateTime.Now.AddYears(-userAge);
+         
+            try
+            {
+                if (userAge < 1)
+                {
+                    Console.WriteLine("Please enter a number greater than zero.");
+                    Console.ReadLine();
+                }
+                
+            } catch (Exception)
+            { 
+                Console.WriteLine("Something else went wrong.");
+                Console.ReadLine();
+            }
+            Console.WriteLine(dobYear.Year);
+            Console.ReadLine();
+        }
+    }
+}
