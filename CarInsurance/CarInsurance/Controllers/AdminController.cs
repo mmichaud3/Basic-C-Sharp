8042e16bcd3a1555d13cacb6b52c3a76bf43b35c
Michael <mmichaud3@yahoo.com>
2019-10-10 17:25:49 -0700

    changes

--- CarInsurance/CarInsurance/Controllers/AdminController.cs
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -15,121 +15,27 @@ namespace CarInsurance.Controllers
         {
             using (Car_InsuranceEntities db = new Car_InsuranceEntities())
             {
-                var today = DateTime.Today;
-                int total = 50;
-
-
-                var customers = db.Customers;//(from c in db.Customers
-                                 //where c.Quote != null
-                                 //select c).ToList();
+                var customers = db.Customers.Where(x => x.Quoted == null).ToList();
 
                 var customerVMs = new List<CustomerVM>();
-                foreach (var customer in customers) // getting age and price additional cost for quote
+                foreach (var customer in customers)
                 {
-                    var dob = Convert.ToDateTime(customer.DateOfBirth);
+                    var customerVM = new CustomerVM();
+                    customerVM.Id = customer.Id;
+                    customerVM.FirstName = customer.FirstName;
+                    customerVM.LastName = customer.LastName;
+                    customerVM.EmailAddress = customer.EmailAddress;
+                    customerVM.Quote = Convert.ToInt32(customer.Quote);
 
 
-                    if (dob.Year > today.Year - 25)
-                    {
-                        total = total + 25;
-                    }
-                    else if (dob.Year > today.Year - 18)
-                    {
-                        total = total + 100;
-                    }
-                    else if (dob.Year < today.Year - 100)
-                    {
-                        total = total + 25;
-                    }
-                    foreach (var customer2 in customers) //getting year of car and additional cost for quote
-                    {
-                        if (customer2.CarYear < 2000)
-                        {
-                            total = total + 25;
-                        }
-                        else if (customer2.CarYear > 2015)
-                        {
-                            total = total + 25;
-                        }
-                    }
-                    foreach (var customer3 in customers) //getting make of car and additional cost for quote
-                    {
-                        if (customer3.CarMake == "Porsche")
-                        {
-                            total = total + 25;
-                        }
-                    }
-                    foreach (var customer4 in customers) //getting model and additional cost for quote
-                    {
-                        if (customer4.CarModel == "911 Carrera")
-                        {
-                            total = total + 25;
-                        }
-                    }
-                    foreach (var customer5 in customers)  //getting number of speeding tickets and additional cost for quote
-                    {
-                        total = total * 10 + total;
-                    }
-                    foreach (var customer6 in customers) //getting  DUI and additinal cost for quote
-                    {
-                        if (customer6.DUI == "yes")
-                        {
-                            total = Convert.ToInt32(total * .25 + total);
-                        }
-                    }
-                    foreach (var customer7 in customers)  // getting full coverage additional cost for quote
-                    {
-                        if (customer7.FullOrLiability == "Full")
-                        {
-                            total = Convert.ToInt32(total * .50 + total);
-
-                        }
-                    }
-
-
-                    customer.Quote = total;
-                    db.Customers.Add(customer);
-                    db.SaveChanges();
-
-                    @ViewBag.Total = total;
+                    customerVMs.Add(customerVM);
                 }
-                return View("Index");
+                return View(customerVMs);
             }
         }
+    }
+}
 
                     
                   
-                    
-                //var customerVM = new CustomerVM();
-                //    customerVM.Quote = ag
-        //            customerVM.Id = customer.Id;
-        //            customerVM.FirstName = customer.FirstName;
-        //            customerVM.LastName = customer.LastName;
-        //            customerVM.EmailAddress = customer.EmailAddress;
-        //            customerVM.DateOfBirth = customer.DateOfBirth;
-        //            customerVM.CarYear = customer.CarYear;
-        //            customerVM.CarMake = customer.CarMake;
-        //            customerVM.CarModel = customer.CarModel;
-        //            customerVM.DUI = customer.DUI;
-        //            customerVM.NumberOfTickets = customer.NumberOfTickets;
-        //            customerVM.FullOrLiability = customer.FullOrLiability;
-        //            customerVM.Quote = customer.Quote;
-        //            customerVMs.Add(customerVM);
-        //        }
-        //        return View(customerVMs);
-
-        //    }
-        //}
-        //public ActionResult Quoted(int Id)
-        //{
-        //    using (Car_InsuranceEntities db = new Car_InsuranceEntities())
-        //    {
-        //        var customerVMs = new List<CustomerVM>();
-        //        foreach (var customer in Customer) 
-        //        {
-        //            if (customer.date)
-        //        }
-        //    }
-        //}
-    }
-}
+    
--- CarInsurance/CarInsurance/Controllers/HomeController.cs
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,20 +42,88 @@ namespace CarInsurance.Controllers
                     customer.DUI = dui;
                     customer.NumberOfTickets = numberOfTickets;
                     customer.FullOrLiability = fullOrLiability;
-                    //customer.Quote = quote;
+                   // customer.Quote = quote;
 
+
+                    var today = DateTime.Today;
+                    int total = 50;
+
+
+                  
                     
+                        var dob = Convert.ToDateTime(customer.DateOfBirth); // getting age and price additional cost for quote
 
 
-                    db.Customers.Add(customer);
-                    db.SaveChanges();
+                    if (dob.Year > today.Year - 25)
+                        {
+                            total = total + 25;
+                        }
+                        else if (dob.Year > today.Year - 18)
+                        {
+                            total = total + 100;
+                        }
+                        else if (dob.Year < today.Year - 100)
+                        {
+                            total = total + 25;
+                        }
+                        
+                        
+                            if (customer.CarYear < 2000) //getting year of car and additional cost for quote
+                    {
+                                total = total + 25;
+                            }
+                            else if (customer.CarYear > 2015)
+                            {
+                                total = total + 25;
+                            }
+                      
+                        
+                            if (customer.CarMake == "Porsche") //getting make of car and additional cost for quote
+                    {
+                                total = total + 25;
+                            }
+                        
+                        
+                            if (customer.CarModel == "911 Carrera") //getting model and additional cost for quote
+                    {
+                                total = total + 25;
+                            }
+                        //}
+                    if (customer.NumberOfTickets > 0)  //getting number of speeding tickets and additional cost for quote
+                    {
+                        total = total + (numberOfTickets * 10);
+                    }
+              
+                           
+                        
+                            if (customer.DUI == "yes") //getting  DUI and additinal cost for quote
+                    {
+                                total = Convert.ToInt32(total * .25 + total);
+                            }
+                       
+                        
+                            if (customer.FullOrLiability == "Full") // getting full coverage additional cost for quote
+                    {
+                                total = Convert.ToInt32(total * .50 + total);
+
+                            }
+                        
+
+                        customer.Quote = total;
+
+                        db.Customers.Add(customer);
+                        db.SaveChanges();
+
+                        @ViewBag.Total = total;
+
+                     
+                    }
+                    return View("Success");
                 }
-                return View("Success");
-            }
 
+            }
         }
 
 
 
     }
-}
